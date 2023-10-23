f275a7e7477d05ed5d29cda8f2a1b6d550d16a55
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-10-23 18:50:44 +0100

    Adding elements to scrips.js file, and first togglePlay() function

--- 11 Custom Video Player/scripts.js
+++ 11 Custom Video Player/scripts.js
@@ -1,3 +1,27 @@
+/* Get Our Elements */
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+/* Build our Functions */
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.paused();
+    }
+};
+
+
+
+/*Hook Up the event Listeners */
+
 
 
 
