1aa496bb93e1ea49e660aa3f61d33ed47af00d69
Miguel Lamas <lamasmigs@yahoo.co.uk>
2024-04-13 17:33:49 +0100

    storing countdown in own global variable

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
+//storing countdown in own global variable
+let countdown;
+
 function timer(seconds) {
     const now = Date.now(); // gives you current time stamp
     const then = now + seconds * 1000;   
-    console.log({now, then});
 
     //need to display every second left by creating setInterval and running it every second
-    setInterval(() => {
-        const secondsLeft = (then - Date.now()) / 1000; // in miliseconds so divide by 1000
-        
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000); // in miliseconds so divide by 1000
+        // check if we should stop it before we display import PropTypes from 'prop-types'
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        console.log(secondsLeft);
     }, 1000);
 };
 
